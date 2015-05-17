adc18cb64d13b45fadfb42430233185467967ef5
Sergey Vasko <sergio.vasko@gmail.com>
2015-05-17 20:03:05 +0300

    Creating a redirection for the admin bundle default route.

--- src/Blog/AdminBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Blog/AdminBundle/Tests/Controller/DefaultControllerTest.php
@@ -4,14 +4,26 @@ namespace Blog\AdminBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * Class DefaultControllerTest
+ */
 class DefaultControllerTest extends WebTestCase
 {
+    /**
+     * Test redirection to post index
+     */
     public function testIndex()
     {
-        /*$client = static::createClient();
+        $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'admin',
+            'PHP_AUTH_PW'   => 'admin',
+        ));
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('GET', '/admin/');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);*/
+        $this->assertTrue(
+            $client->getResponse()->isRedirect('/admin/post/'),
+            'There was no redirection to the posts index'
+        );
     }
 }
